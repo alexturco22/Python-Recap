bb163accc62e41d1edc0f328399a4c90078b0f98
Alex <alex_turco1@baylor.edu>
2024-02-06 22:11:16 -0600

    updated gitignore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,9 +1,9 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import matplotlib.pyplot as plt         #Step 1) Create a Virtual Environment (VE) in terminal
+import numpy as np                              #for pc's use... py -3 -m venv (and then name of VE)
 
-x = np.linspace(0, 20, 100)
-plt.plot(x, np.sin(x))
+x = np.linspace(0, 20, 100)             #Step 3) Install 3rd party library/module bc there was an error "No module named matplotlib"
+plt.plot(x, np.sin(x))                          #for pc's use... pip3 install (whatever library)
+                                        #Step 2) Activate the VE
+plt.show()                                      #for pc's use... myvenv/Scripts/activate
 
-plt.show()
-
-print("Hi there!")
+print("Hello World!")             #Won't work if you download with root interpreter vs the virtual environment.
